01da874227b8f33bf0a5ee92f65b47ca7a170889
acrylic-style <me@acrylicstyle.xyz>
2019-05-22 20:34:32 +0900

    get prefix from config

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const dispatcher = require('bot-framework/dispatcher')
 const client = new discord.Client()
 const _fs = require('fs')
 const fs = _fs.promises
-const prefix = '\\'
 const config = require('./config.yml')
-const cache = { guilds: 0, messagecount: null }
+const { prefix } = config
+const cache = { guilds: 0, messagecount: null, beta: false }
+const git = require('simple-git/promise')
 
 if (!_fs.existsSync('./messagecount.json')) _fs.writeFileSync('./messagecount.json', '[]')
 if (!_fs.existsSync('./banmessagecount.json')) _fs.writeFileSync('./banmessagecount.json', '[]')
@@ -17,12 +18,15 @@ if (!_fs.existsSync('./banmessagecount.json')) _fs.writeFileSync('./banmessageco
 const messagecount = require('./messagecount.json')
 const banmessagecount = require('./banmessagecount.json')
 
-client.on('ready', () => {
+client.on('ready', async () => {
   client.user.setActivity(`${prefix}help | ${client.guilds.size} servers`)
   cache['guilds'] = client.guilds.size
   logger.info('Bot is ready!')
     .info('   Server Name | Server ID | MemberCount')
-    .info(client.guilds.map(guild => ` - ${guild.name} | ${guild.id} | ${guild.memberCount}人`).join('\n'))
+    .info(client.guilds.map(guild => ` - ${guild.name} | ${guild.id} | ${guild.memberCount} users`).join('\n'))
+  const status = await git().branchLocal()
+  if (status.current) cache.beta = true
+  if (cache.beta) logger.config(true).debug('Running in beta mode. Some features will be disabled.')
 })
 
 client.on('error', error => logger.error(error.stack || error))
@@ -36,6 +40,7 @@ client.on('message', async message => {
   if (!message.guild) return
   cache['messagecount'] = messagecount
   if (message.guild.me.nickname !== null) message.guild.me.setNickname(`${message.guild.memberCount}人`)
+  if (cache.beta) return
   if (!banmessagecount[message.channel.id]) banmessagecount[message.channel.id] = { banmessage: 0 }
   const BCM = banmessagecount[message.channel.id]
   const result = messagecount.filter(m => BCM.banmessage === 0 && m.guildid === message.guild.id)
