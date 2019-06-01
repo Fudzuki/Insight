8b355112e9b1dd6af01864c0b737c8f0fdcac033
acrylic-style <me@acrylicstyle.xyz>
2019-06-01 20:09:27 +0900

    daily refactoring

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 #!/usr/local/bin/node
 require('./src/yaml')
-// Discord bot implements
-const discord = require('discord.js')
+const Discord = require('discord.js')
 const logger = require('logger.js').LoggerFactory.getLogger('main', 'green')
 const dispatcher = require('bot-framework/dispatcher')
-const client = new discord.Client()
+const client = new Discord.Client()
 const _fs = require('fs')
 const fs = _fs.promises
+const prefix = '\\'
 const config = require('./config.yml')
-const { prefix } = config
-const cache = { guilds: 0, messagecount: null, beta: false }
-const git = require('simple-git/promise')
+const cache = { guilds: 0, messagecount: null }
 
 if (!_fs.existsSync('./messagecount.json')) _fs.writeFileSync('./messagecount.json', '[]')
 if (!_fs.existsSync('./banmessagecount.json')) _fs.writeFileSync('./banmessagecount.json', '[]')
+//if (!_fs.existsSync('./usercount.json')) _fs.writeFileSync('./usercount.json', '[]') unused
 
 const messagecount = require('./messagecount.json')
-const banmessagecount = require('./banmessagecount.json')
+const bm = require('./banmessagecount.json')
+//const umc = require('./usercount.json') unused
 
-client.on('ready', async () => {
+client.on('ready', () => {
   client.user.setActivity(`${prefix}help | ${client.guilds.size} servers`)
   cache['guilds'] = client.guilds.size
   logger.info('Bot is ready!')
     .info('   Server Name | Server ID | MemberCount')
-    .info(client.guilds.map(guild => ` - ${guild.name} | ${guild.id} | ${guild.memberCount} users`).join('\n'))
-  const status = await git().branchLocal()
-  if (status.current) cache.beta = true
-  if (cache.beta) logger.config(true).debug('Running in beta mode. Some features will be disabled.')
+    .info(client.guilds.map(guild => ` - ${guild.name} | ${guild.id} | ${guild.memberCount}人`).join('\n'))
 })
 
 client.on('error', error => logger.error(error.stack || error))
@@ -37,20 +34,31 @@ client.on('message', async message => {
 
   if (message.content.startsWith(prefix)) dispatcher(message, require('./lang/ja.json'), prefix, config.owners, prefix)
 
-  if (!message.guild) return
-  cache['messagecount'] = messagecount
-  if (message.guild.me.nickname !== null) message.guild.me.setNickname(`${message.guild.memberCount}人`)
-  if (cache.beta) return
-  if (!banmessagecount[message.channel.id]) banmessagecount[message.channel.id] = { banmessage: 0 }
-  const BCM = banmessagecount[message.channel.id]
-  const result = messagecount.filter(m => BCM.banmessage === 0 && m.guildid === message.guild.id)
-  if (result.length) result.messagecount++
+  //if (!message.guild) return
+  //cache['messagecount'] = messagecount
+  //if (message.guild.me.nickname !== null) message.guild.me.setNickname(`${message.guild.memberCount}人`)
+  // if (!banmessagecount[message.channel.id]) banmessagecount[message.channel.id] = { banmessage: 0 }
+  // const BCM = banmessagecount[message.channel.id]
+  //const result = messagecount.filter(m => BCM.banmessage === 0 && m.guildid === message.guild.id)
+  //if (result.length) result.messagecount++
+  if(!bm[message.channel.id]) bm[message.channel.id] = { banmessage: 0 }
+  /*
+  for(const u of umc){
+    if(u.uid !== message.author.id && u.gid !== message.guild.id){
+      umc.push({
+        gid: message.guild.id,
+        uid: message.author.id,
+        UserCounts: 0,
+      })
+    }
+  }*/// because umc is unused
 
   for(const abc in messagecount){
-    if(!client.channels.has(messagecount[abc].channelid)){
+    if(!client.channels.has(messagecount[abc].channelid)) {
       messagecount[abc].guildid = 0
     } else {
-      if(messagecount[abc].guildid == message.guild.id){
+      if(messagecount[abc].guildid == message.guild.id && bm[message.channel.id].banmessage == 0){
+        messagecount[abc].messagecount++
         client.channels.get(messagecount[abc].channelid).setName(`${messagecount[abc].messagename}: ${messagecount[abc].messagecount}`)
       }
     }
