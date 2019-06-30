6dab6aa5d17cc6713ad96eafa875d57a14311c33
acrylic-style <me@acrylicstyle.xyz>
2019-06-30 15:03:17 +0900

    re-add invite command with useless message

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+const { Command } = require('bot-framework')
+
+module.exports = class extends Command {
+  constructor() {
+    super('invite')
+  }
+
+  async run(msg) {
+    msg.channel.send(`https://discordapp.com/oauth2/authorize?client_id=${msg.client.user.id}&permissions=8&scope=bot`)
+  }
+}
